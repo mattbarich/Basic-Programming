dc7a7d5ee1b46b2a2b106c7d65cf57bd1e86b7db
mattbarich <mattbarich1@gmail.com>
2024-03-11 20:55:04 -0600

    Simple Bubble Sort

---
+++ C/Resources/bubble_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// perform the bubble sort
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
+}
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9, 12, -2, 24562, 123, 123, 162, 87, 45};
+  
+  int size = sizeof(data) / sizeof(data[0]);
+
+  printf("Unsorted Array\n");
+  printArray(data, size);
+
+  bubbleSort(data, size);
+  
+  printf("Sorted Array in Ascending Order:\n");
+  printArray(data, size);
+}

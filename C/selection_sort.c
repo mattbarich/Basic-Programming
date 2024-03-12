31d6f3a315b0c24617f939aaf4f0af49f041080a
mattbarich <mattbarich1@gmail.com>
2024-03-11 21:27:26 -0600

    Basic Selection Sort.

---
+++ C/selection_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// function to swap the the position of two elements
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int array[], int size){
+    for (int i=0; i<size-1; i++){
+        int min_index = i;
+        for (int j=i+1; j<size; j++){
+            if(array[j] < array[min_index]){
+                min_index = j;
+            }
+        }
+        swap(&array[min_index], &array[i]);
+    }
+}
+
+// function to print an array
+void printArray(int array[], int size){
+    for (int i = 0; i < size; ++i)    {
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+
+// driver code
+int main() {
+
+    int data[] = {-2, 45, 0, 11, -9, 12, -2, 24562, 123, 123, 162, 87, 45};
+    int size = sizeof(data) / sizeof(data[0]);
+
+    printf("Unsorted Array\n");
+    printArray(data, size);
+
+    selectionSort(data, size);
+
+    printf("Sorted Array in Ascending Order:\n");
+    printArray(data, size);
+
+}
